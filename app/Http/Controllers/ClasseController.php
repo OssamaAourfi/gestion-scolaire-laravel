a8a074af1b1d16b9ae8774854dc03f5722493e1a
OssamaAourfi <aourfidev@gmail.com>
2025-12-10 17:19:30 +0100

    first commit

---
+++ app/Http/Controllers/ClasseController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Classe;
+use Illuminate\Http\Request;
+
+class ClasseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $classes = Classe::orderBy('created_at', 'desc')->paginate(5);
+        return view('classes.index', compact('produits'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('classes.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nom' => 'required|unique:classes,nom',
+            'niveau' => 'required',
+        ]);
+
+        Classe::create($request->all());
+
+        return redirect()->route('classes.index')
+            ->with('success', 'Classe ajoutée avec succès');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $produit = Classe::findOrFail($id); // findOrFail bach tila makanx y3ti 404
+        return view('classes.edit', compact('classe'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'nom' => 'required',
+            'niveau' => 'required',
+        ]);
+
+        $classes = Classe::findOrFail($id);
+        $classes->update($request->all());
+
+        return redirect()->route('classes.index')->with('success', 'Modifié avec succès!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $classes = Classe::findOrFail($id);
+        $classes->delete();
+        return redirect()->route('classes.index')->with('success', 'Supprimé!');
+    }
+}
